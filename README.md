0aa37b8cc54fad2700e1bf6aeaf01a949f99fd09
AnthonyToons <anthonyschuurmans4@gmail.com>
2023-05-24 21:08:15 +0200

    Setup
    Created all files needed for the base, along with a small input form.
    
    Tomorrow: CRUD

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Spending-portal
 The spending portal is a forum where you can insert your daily spendings, see your average spendings based on a filter, and make notes of your spendings.
+
+Create an account with ease, see your avergae spendings and add new spendings.
+Add notes for yourself, make a budgeting plan (or not)!
+Everything to keep track of what you're spending!
---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <div class="login-box">
+            <form action="crud-pages/process-login.php">
+                <input type="text" name="username" placeholder="username" required>
+                <input type="email" name="email" placeholder="email" required>
+                <input type="password" name="password" placeholder="password" required>
+                <input type="submit" value="Login">
+            </form>
+            <!-- User can create an account if they don't have one -->
+            <div class="no-account">
+                <p>Don't have an account?</p>
+                <a href="crud-pages/create-account.php" class="button button-blue">Make one!</a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
