29f627320879adc72d344b529663681563c338cf
AnthonyToons7 <anthonyschuurmans4@gmail.com>
2023-05-28 22:56:02 +0200

    Added JS for statistics

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded",()=>{
+    const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+    if (window.location.href.indexOf("dashboard") == -1) return;
+    for (let i=0;i<12;i++) {
+        const element = document.createElement("div");
+        element.classList.add(months[i]);
+        document.querySelector(".stats-container").appendChild(element);
+    }
+});
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -9,22 +9,44 @@
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <header></header>
+    <header>
+        <ul class="nav mobile-nav d-flex flex-column mb-4">
+            <li>Dashboard</li>
+            <li>Create new</li>
+            <li>Add note</li>
+            <li>Account</li>
+            <li>Log out</li>
+        </ul>
+    </header>
     <main>
-        <section class="sidebar d-flex flex-column">
-            <ul>
-                <li>Dashboard</li>
-                <li>Create new</li>
-                <li>Add note</li>
-                <li>Account</li>
-            </ul>
-            <ul>
-                <li>Log out</li>
-            </ul>
-        </section>
-        <section class="content"></section>
+        <div class="content">
+            <section class="sidebar d-flex flex-column col-3">
+                <ul>
+                    <li>Dashboard</li>
+                    <li>Create new</li>
+                    <li>Add note</li>
+                    <li>Account</li>
+                </ul>
+                <ul>
+                    <li>Log out</li>
+                </ul>
+            </section>
+            <section class="dashboard-content col-9">
+                <div class="content">
+                    <div class="statistics col-5">
+                        <div class="d-grid">
+                            <div class="col-1"></div>
+                            <div class="col-11 stats-container"></div>
+                            <div class="col-11"></div>
+                        </div>
+                    </div>
+                    <div class="recent-spendings col-5"></div>
+                </div>
+            </section>
+        </div>
     </main>
     <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="../js/script.js"></script>
 </body>
 </html>
