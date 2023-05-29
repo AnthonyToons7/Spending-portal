c4cb66a23efb9ce627125a8f45a800e5ed28829b
AnthonyToons7 <anthonyschuurmans4@gmail.com>
2023-05-29 13:01:48 +0200

    Small changes
    Added jquery, small changes to js and some prep

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded",()=>{
     for (let i=0;i<12;i++) {
         const element = document.createElement("div");
         element.classList.add(months[i]);
+        element.textContent = months[i];
         document.querySelector(".stats-container").appendChild(element);
     }
 });
